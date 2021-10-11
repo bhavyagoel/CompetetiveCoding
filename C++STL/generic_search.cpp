f23835a22f9a607ea97e39e027080f68414c8c71
Bhavya Goel <bgoel4132@gmail.com>
2021-10-12 01:12:51 +0530

    Add: Itrerator to make work on any type of container

--- C++STL/generic_search.cpp
+++ C++STL/generic_search.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include<list> 
 using namespace std; 
 
-
+/*
 template<typename T>
 int search(T arr[], int n, T key) {
     for(int i = 0; i < n; i++) {
@@ -11,27 +12,56 @@ int search(T arr[], int n, T key) {
         return n;
     }
 }
+*/
 
-// int search(int arr[], int n, int key) {
-//     for(int i = 0; i < n; i++) {
-//         if(arr[i] == key) {
-//             return i;
-//         }
-//         return n;
-//     }
-// }
+template<class ForwardIterator, class T>
+ForwardIterator search(ForwardIterator start, ForwardIterator end, T key){
+
+    while(start!= end) {
+        if(*start==key){
+            return start;
+        }
+        start++;
+    }
+    return end;
+}
+
+/*
+int search(int arr[], int n, int key) {
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == key) {
+            return i;
+        }
+        return n;
+    }
+}
+*/
 
 int main() {
+    /*
     int arr[] = {1,2,3,4,10};
-    // int n = sizeof(arr)/sizeof(int); 
-    // int key = 10; 
+    int n = sizeof(arr)/sizeof(int); 
+    int key = 10; 
 
-    // cout<<search(arr, n, key)<<endl;
+    cout<<search(arr, n, key)<<endl;
 
 
     float b[] = {1.1, 1.2, 1.3};
     int n = sizeof(b)/sizeof(float); 
     float key = 1.1; 
     cout<<search(b, n, key)<<endl;
+    */
 
+    list<int> l; 
+    l.push_back(1);
+    l.push_back(2);
+    l.push_back(4);
+    l.push_back(5);
+    l.push_back(7);
+    l.push_back(8);
+
+    auto it = search(l.begin(), l.end(), 5);
+    if(it==l.end()){
+        cout<<"ELement not present"<<endl;
+    }else cout<<*it<<endl;
 }
